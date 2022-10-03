3b501ec2e630ef872a7130f9d483302049384b53
TMC\mehrnoosh.rigi <mehrnoosh.rigi@tmc.nl>
2022-10-03 16:31:48 +0200

    Hibernate - update

---
+++ src/hibernate/demo/entity/UpdateStudentDemo.java
@@ -0,0 +1,31 @@
+package hibernate.demo.entity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class UpdateStudentDemo {
+    public static void main(String args[]){
+        //  create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+        // create session
+        Session session = factory.getCurrentSession();
+        try {
+            int id = 1;
+            // Get new session and start transaction
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+            // retrieve student based on ID on primary key
+            System.out.println("\n getting student with ID" + id);
+            Student myStudent = session.get(Student.class, id);
+            myStudent.setFirstName("Updated name");
+            // commit the transaction
+            session.getTransaction().commit();
+        }finally {
+            factory.close();
+        }
+    }
+}
